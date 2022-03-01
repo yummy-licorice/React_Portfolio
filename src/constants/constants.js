4f43f8dac5366644ee5299728cddcef5c5c467c1
Infinitybeond1 <Infinitybeond1@protonmail.com>
2022-03-01 00:00:07 -0600

    add some projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,18 +1,18 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
+    title: 'BrickBot',
+    description: "A multipurpose discord bot I created using nodejs with discordjs (The project is no longer maintained)",
+    image: '/images/1.png',
+    tags: ['Mongo ', 'Node ', 'DiscordJS ', 'Axios'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    title: 'Snake.py',
+    description:"A retro style snake game I created simply using pythons TurtleGraphics library",
     image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
+    tags: ['Python', 'TurtleGraphics', 'Tkinter'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 1,
@@ -42,5 +42,5 @@ export const TimeLineData = [
   { year: 2019, text: 'Learned NodeJs', },
   { year: 2020, text: 'Created a Discord Bot', },
   { year: 2021, text: 'Learn ReactJs and NextJs', },
-  { year: 2022, text: 'Started making beutiful websites with React', },
+  { year: 2022, text: 'Started learning Python, Rust, and Dart', },
 ];
